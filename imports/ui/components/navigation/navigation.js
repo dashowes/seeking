e3d2608e978d7b0dae947fa1258a54ef88d39312
Jim <das.howes@gmail.com>
2016-12-13 20:55:36 +0000

    I hate css

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -2,12 +2,14 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
  
 import template from './navigation.html';
+import { name as ProfilesList } from '../profilesList/profilesList';
  
 const name = 'navigation';
  
 // create a module
 export default angular.module(name, [
-  angularMeteor
+  angularMeteor,
+  ProfilesList
 ]).component(name, {
   template,
   controllerAs: name
