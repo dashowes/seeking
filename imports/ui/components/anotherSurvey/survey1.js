944f13e0ff8ced10cfbeb4787eb0391d5b0e5f21
Jim <das.howes@gmail.com>
2017-01-04 22:39:33 +0000

    edgy

--- imports/ui/components/anotherSurvey/survey1.js
+++ imports/ui/components/anotherSurvey/survey1.js
@@ -54,6 +54,41 @@ class Survey1 {
         skillLevelAnswer1: this.profile.skillLevelAnswer1,
         skillLevelAnswer2: this.profile.skillLevelAnswer2,
         skillLevelWeight: this.profile.skillLevelWeight,
+        liveBandAnswer1: this.profile.liveBandAnswer1,
+        liveBandAnswer2: this.profile.liveBandAnswer2,
+        liveBandWeight: this.profile.liveBandWeight,
+        gearAnswer1: this.profile.gearAnswer1,
+        gearAnswer2: this.profile.gearAnswer2,
+        gearWeight: this.profile.gearWeight,
+        priorityAnswer: this.profile.priorityAnswer,
+        priorityWeight: this.profile.priorityWeight,
+        productionAnswer: this.profile.productionAnswer,
+        productionWeight: this.profile.productionWeight,
+        moneyAnswer1: this.profile.moneyAnswer1,
+        moneyAnswer2: this.profile.moneyAnswer2,
+        moneyWeight: this.profile.moneyWeight,
+        practiceTimeAnswer: this.profile.practiceTimeAnswer,
+        practiceTimeWeight: this.profile.practiceTimeWeight,
+        localShowsAnswer: this.profile.localShowsAnswer,
+        localShowsWeight: this.profile.localShowsWeight,
+        touringAnswer: this.profile.touringAnswer,
+        touringWeight: this.profile.touringWeight,
+        recordingAnswer: this.profile.recordingAnswer,
+        recordingWeight: this.profile.recordingWeight,
+        songwritingAnswer1: this.profile.songwritingAnswer1,
+        songwritingAnswer2: this.profile.songwritingAnswer2,
+        songwritingWeight: this.profile.songwritingWeight,
+        originalOrCoverAnswer: this.profile.originalOrCoverAnswer,
+        originalOrCoverWeight: this.profile.originalOrCoverWeight,
+        occasionalCoverAnswer: this.profile.occasionalCoverAnswer,
+        occasionalCoverWeight: this.profile.occasionalCoverWeight,
+        venuesAnswer: this.profile.venuesAnswer,
+        venuesWeight: this.profile.venuesWeight,
+        venuesBadAnswer: this.profile.venuesBadAnswer,
+        venuesBadWeight: this.profile.venuesBadWeight,
+        practiceSpaceAnswer1: this.profile.practiceSpaceAnswer1,
+        practiceSpaceAnswer2: this.profile.practiceSpaceAnswer2,
+        practiceSpaceWeight: this.profile.practiceSpaceWeight,
       }
     }, (error) => {
       if (error) {
@@ -85,7 +120,16 @@ function config($stateProvider) {
   
   $stateProvider
     .state('survey1', {
-      url: '/survey1',
-      template: '<survey1></survey1>'
-    });
+      url: '/survey1/:profileId',
+      template: '<survey1></survey1>',
+      resolve: {
+      currentUser($q) {
+        if (Meteor.userId() === null) {
+          return $q.reject('AUTH_REQUIRED');
+        } else {
+          return $q.resolve();
+        }
+      }
+    }
+  });
 }
