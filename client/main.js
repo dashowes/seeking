bdd6acb2bca1c0b880412374d9024f581a3b8cbe
Jim <das.howes@gmail.com>
2017-02-01 20:01:36 +0000

    BYAAAAAH

--- client/main.js
+++ client/main.js
@@ -11,6 +11,17 @@ function onReady() {
     strictDi: true
   });
 }
+
+// Meteor.startup(function() {
+//   Template.fb_pic.pic = function() {// helper function to display the pic on the page
+//     var userProfile;
+//     userProfile = Meteor.user().profile;
+
+//     if(userProfile) { // logic to handle logged out state
+//       return userProfile.picture;
+//     }
+//   };
+// });
  
 if (Meteor.isCordova) {
   angular.element(document).on('deviceready', onReady);
--- server/startup/environments.js
+++
@@ -1,3 +0,0 @@
-Meteor.startup(function () {
-    process.env.MAIL_URL="smtp://das.howes@gmail.com:Jfbrgr85!@smtp.gmail.com:465/";
-})
